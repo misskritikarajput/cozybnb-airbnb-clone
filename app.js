4dc846702fcca2fc31d931c97d453dfadccb1ce7
Kritika Rajput <127962807+misskritikarajput@users.noreply.github.com>
2025-06-25 18:15:33 +0530

    mongoose ulrl change for deployment

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ main()
     })
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/cozyBnB');
+   await mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 }
 
 app.set("view engine", "ejs");
